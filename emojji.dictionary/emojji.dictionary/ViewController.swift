c889c7054eecaa2ecb492cb4497053bd4381a2c6
Marcin Zgola <marcinzgola@Marcins-MacBook-Pro.local>
2016-10-11 21:54:26 -0500

    real first commit

---
+++ emojji.dictionary/emojji.dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emojji.dictionary
+//
+//  Created by Marcin Zgola on 10/11/16.
+//  Copyright © 2016 Udemy lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
